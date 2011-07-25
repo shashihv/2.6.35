e818af04d849a81c41de3aab7f9061ba7d12e07d
root <root@francisco.(none)>
2011-07-25 02:58:36 +0100

    Modify SmartAss to use a high priority workqueue. From intersectRaven.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 13 */
+/* This file is auto generated, version 15 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#13 PREEMPT Tue Jul 19 19:32:09 WEST 2011"
-#define LINUX_COMPILE_TIME "19:32:09"
+#define UTS_VERSION "#15 PREEMPT Mon Jul 25 02:54:51 WEST 2011"
+#define LINUX_COMPILE_TIME "02:54:51"
 #define LINUX_COMPILE_BY "francisco"
 #define LINUX_COMPILE_HOST "francisco"
 #define LINUX_COMPILE_DOMAIN "(none)"
